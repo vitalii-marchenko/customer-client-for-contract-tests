9883a9407158c1b7b00f3c65df2a1c5317f28701
vitaliimarchenko <vitalii.marchenko@auto1.com>
2022-04-19 09:18:30 +0300

    Cosmetic fix

--- src/main/java/com/example/customerclient/CustomerClient.java
+++ src/main/java/com/example/customerclient/CustomerClient.java
@@ -1,11 +1,8 @@
 package com.example.customerclient;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,7 +17,7 @@ public class CustomerClient {
     }
 
     public Collection<Customer> getAllCustomers() {
-        ParameterizedTypeReference<Collection<Customer>> ptr = new ParameterizedTypeReference<Collection<Customer>>() { };
+        ParameterizedTypeReference<Collection<Customer>> ptr = new ParameterizedTypeReference<>() { };
         ResponseEntity<Collection<Customer>> responseEntity = restTemplate.exchange("http://localhost:8081/customers",
                 HttpMethod.GET, null, ptr);
         return responseEntity.getBody();
